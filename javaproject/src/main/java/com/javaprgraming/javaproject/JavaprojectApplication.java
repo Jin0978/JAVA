804ae9b511e8891b8fa55f57c47cb431f88db596
Jin0978 <pdgdt00@gmail.com>
2025-12-06 21:24:34 +0900

    fix: 최최종버전

--- javaproject/src/main/java/com/javaprgraming/javaproject/JavaprojectApplication.java
+++ javaproject/src/main/java/com/javaprgraming/javaproject/JavaprojectApplication.java
@@ -1,11 +1,20 @@
 package com.javaprgraming.javaproject;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.TimeZone;
 
 @org.springframework.scheduling.annotation.EnableScheduling // ⭐ [추가] 스케줄러 활성화
 @SpringBootApplication
 public class JavaprojectApplication {
+
+	@PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("✅ 현재 시간대: " + TimeZone.getDefault().getID());
+        System.out.println("✅ 현재 시간: " + java.time.LocalDateTime.now());
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaprojectApplication.class, args);
